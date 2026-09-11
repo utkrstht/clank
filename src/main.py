dcf12d7559034b237d2701cc601052f54bdee91b
utkrishth <utkrishth@utkrishth.in>
2026-09-11 20:43:47 +0800

    feat: rejection reasons dictionary

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import re
 RAW_README_REGEX = r"^https?:\/\/(?:raw\.githubusercontent\.com\/[^\/]+\/[^\/]+\/[^\/]+|gitlab\.com\/api\/v4\/projects\/[^\/]+\/repository\/files\/README(?:\.[a-zA-Z0-9]+)?\/raw|bitbucket\.org\/[^\/]+\/[^\/]+\/raw\/[^\/]+)\/README(?:\.[a-zA-Z0-9]+)?$"
 
 # TODO: add all possible rejection reasons to dictionary 
+rejection_reasons = {"raw_readme":"Your raw README link is not raw, please update it.", "ai_readme":"Your README appears to be AI-generated, please re-write it by hand.", "ai_code":"Your codebase appears to be heavily AI-generated with little human effort, please re-write most of your code by hand.", "404_repo": "Your repository is either non-existent or privated, please fix this issue.", "404_demo": "Your demo link is either non-existent or private, please fix this issue", "banned_hosting_provider": "Your demo is hosted on an On-Activity Wake-Up hosting service, these take a very long time to load, please switch to an Always-On hosting service such as Hack Club Nest, Railway and Vercel.", "short_readme": "Your README lacks detail, please add more details such as, how you made it, why you made it, screenshots, features and anything else you wish to add."}
 
 parser = argparse.ArgumentParser(description="clank it up 🚀✨ (this is a joke)")
 
