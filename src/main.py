3833b6e5776111fb31530ead032f6cfac884de3f
utkrishth <utkrishth@utkrishth.in>
2026-09-11 21:15:07 +0800

    feat: create custom rejection message based on checks

--- src/main.py
+++ src/main.py
@@ -4,8 +4,19 @@ import re
 # holy shit
 RAW_README_REGEX = r"^https?:\/\/(?:raw\.githubusercontent\.com\/[^\/]+\/[^\/]+\/[^\/]+|gitlab\.com\/api\/v4\/projects\/[^\/]+\/repository\/files\/README(?:\.[a-zA-Z0-9]+)?\/raw|bitbucket\.org\/[^\/]+\/[^\/]+\/raw\/[^\/]+)\/README(?:\.[a-zA-Z0-9]+)?$"
 
-# TODO: add all possible rejection reasons to dictionary 
+# rejection reasons dictionary 
 rejection_reasons = {"raw_readme":"Your raw README link is not raw, please update it.", "ai_readme":"Your README appears to be AI-generated, please re-write it by hand.", "ai_code":"Your codebase appears to be heavily AI-generated with little human effort, please re-write most of your code by hand.", "404_repo": "Your repository is either non-existent or privated, please fix this issue.", "404_demo": "Your demo link is either non-existent or private, please fix this issue", "banned_hosting_provider": "Your demo is hosted on an On-Activity Wake-Up hosting service, these take a very long time to load, please switch to an Always-On hosting service such as Hack Club Nest, Railway and Vercel.", "short_readme": "Your README lacks detail, please add more details such as, how you made it, why you made it, screenshots, features and anything else you wish to add."}
+
+def create_rejection_message(rejection_reasons):
+    template = """Hello, your project has the following issues, please fix them for approval:
+    ADD_REASONS_HERE 
+    If you have any questions, DM @kaboom or create a ticket in #ask-the-shipwrights. """\
+
+    # seperate rejection reasons with dashes
+    bullet_rejection = "\n".join(f"- {item}" for item in rejection_reasons)
+
+    template = template.replace("ADD_REASONS_HERE", bullet_rejection)
+        
 
 parser = argparse.ArgumentParser(description="clank it up 🚀✨ (this is a joke)")
 
