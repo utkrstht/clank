4f1a9f6c5485b3e9a70888ed66a85d699dbeab9d
utkrishth <utkrishth@utkrishth.in>
2026-09-12 17:02:41 +0800

    fix: edge case in is_banned_domain

--- src/utils.py
+++ src/utils.py
@@ -15,7 +15,7 @@ def calculate_ratelimit(response):
     return sleep_time
 
 def is_banned_domain(demo):
-    if "onrender." in demo or "streamlit." in demo or "hf." in demo or "huggingface" in demo or "ngrok" in demo or "cloudflared" in demo:
+    if "onrender." in demo or "streamlit." in demo or "hf." in demo or "huggingface." in demo or "ngrok" in demo or "cloudflared" in demo:
         return True
     else:
         return False
