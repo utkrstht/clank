d6ededb6d5e307a1d60d7e439cd7dd350b7c5ec7
utkrishth <utkrishth@utkrishth.in>
2026-09-14 19:53:15 +0800

    fix: inverted ratelimit logic because I Am A Dumbass

--- src/checks.py
+++ src/checks.py
@@ -215,7 +215,7 @@ def run_all_checks(readme, repo, demo, stardance):
     # handle ratelimits and also some more of this one liner wizardry
     if readme_response.status_code == 403 and int(readme_response.headers.get("X-RateLimit-Remaining") if not None else 0) == 0:
         ratelimit = calculate_ratelimit(readme_response)
-        if not ratelimit:
+        if ratelimit:
             sleep(ratelimit)
             # I am so smart and proud that I know this one line wizard bullshit
             readme_response = requests.get(readme, timeout=30, headers=headers if GITHUB_AUTH else {})
@@ -223,21 +223,21 @@ def run_all_checks(readme, repo, demo, stardance):
             pass # TODO: handle X-RateLimit-Reset not being provided
     if repo_response.status_code == 403 and int(repo_response.headers.get("X-RateLimit-Remaining") if not None else 0) == 0:
         ratelimit = calculate_ratelimit(repo_response)
-        if not ratelimit:
+        if ratelimit:
             sleep(ratelimit)
             repo_response = requests.get(repo, timeout=30, headers=headers if GITHUB_AUTH else {})
         else:
             pass # TODO: handle X-RateLimit-Reset not being provided
     if demo_response.status_code == 403 and int(demo_response.headers.get("X-RateLimit-Remaining") if not None else 0) == 0:
         ratelimit = calculate_ratelimit(demo_response)
-        if not ratelimit:
+        if ratelimit:
             sleep(ratelimit)
             demo_response = requests.get(demo, timeout=30)
         else:
             pass # TODO: handle X-RateLimit-Reset not being provided
     if stardance_response.status_code == 403 and int(stardance_response.headers.get("X-RateLimit-Remaining") if not None else 0) == 0:
         ratelimit = calculate_ratelimit(stardance_response)
-        if not ratelimit:
+        if ratelimit:
             sleep(ratelimit)
             stardance_response = requests.get(stardance, timeout=30)
         else:
