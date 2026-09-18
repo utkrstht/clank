7b7e978f5e112accb9c88ee65e4af33a339c73a0
utkrishth <utkrishth@utkrishth.in>
2026-09-18 22:18:07 +0800

    feat(slack): run checks on specific fiona certificate via slack

--- src/slack.py
+++ src/slack.py
@@ -9,8 +9,21 @@ load_dotenv()
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 @app.message("!review")
-def handle_review(say):
+def handle_review(message, say):
     say("ok :thumbsup:")
+    text = message["text"]
+    parts = text.split(maxsplit=1)
+
+    if len(parts) < 2:
+        say("yo ass forgot the link :joy:")
+        return
+
+    fiona = parts[1]
+    reject_message = main()
+    
+    if reject_message:
+        say(reject_message) # TODO: handle uploading and sending proof video 
+
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
