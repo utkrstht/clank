33bab70d36fee27eabf57dce00688c0c4c2b416d
utkrishth <utkrishth@utkrishth.in>
2026-09-18 21:31:18 +0800

    refactor(slack/main): shift slack running logic back and allow fiona to be passed as a argument for main

--- src/main.py
+++ src/main.py
@@ -2,11 +2,8 @@ from checks import run_all_checks
 from utils import create_rejection_message
 from video import create_video
 from fiona import get_cert
-from slack import app
 
-from slack_bolt.adapter.socket_mode import SocketModeHandler
 import argparse
-import os
         
 parser = argparse.ArgumentParser(description="clank it up 🚀✨ (this is a joke)")
 
@@ -19,8 +16,20 @@ parser.add_argument("--demo", type=str, help="Demo Link")
 
 args = parser.parse_args()
 
-def main():
-    if not args.fiona:
+def main(fiona=""):
+    if fiona:
+        cert = get_cert(fiona)
+        stardance = f"https://stardance.hackclub.com/projects/{cert['externalId']}"
+
+        reject_reasons = run_all_checks(cert['readmeUrl'], cert['repoUrl'], cert['demoUrl'], stardance)
+
+        if len(reject_reasons) != 0:
+            reject_message = create_rejection_message(reject_reasons)
+            create_video(cert['repoUrl'], stardance, cert['demoUrl'])
+
+            return reject_message
+        
+    elif not args.fiona:
         reject_reasons = run_all_checks(args.readme, args.repo, args.demo, args.stardance)
 
         if len(reject_reasons) != 0:
@@ -29,6 +38,7 @@ def main():
             return reject_message
         else:
             return "All checks passed"
+        
     elif args.fiona:
         cert = get_cert(args.fiona)
         stardance = f"https://stardance.hackclub.com/projects/{cert['externalId']}"
@@ -40,7 +50,4 @@ def main():
             create_video(cert['repoUrl'], stardance, cert['demoUrl'])
 
             return reject_message
-
-if __name__ == "__main__":
-    handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
-    #print(main())
+        
--- src/slack.py
+++ src/slack.py
@@ -1,11 +1,17 @@
 from slack_bolt import App
 from dotenv import load_dotenv
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from main import main
 import os
 
 load_dotenv()
 
 app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
-@app.message("review")
+@app.message("!review")
 def handle_review(say):
     say("ok :thumbsup:")
+
+if __name__ == "__main__":
+    handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
+    handler.start()
